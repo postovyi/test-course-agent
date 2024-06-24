f398350bb5d9374297537902884c1bd28fc1e582
klychliiev <klychliiev@knu.ua>
2024-06-24 15:35:50 +0300

    added typing

--- prompting_solution.py
+++ prompting_solution.py
@@ -4,10 +4,11 @@ from langchain_openai import ChatOpenAI
 from PyPDF2 import PdfReader
 
 from config import settings
-from prompts import prompts 
+from prompts import prompts
 
 
 class CourseAnalyzer:
+    """Class for analyzing user profiles and recommending the course to the selected users based on their jobs and interests"""
 
     def __init__(self):
         self.pdf_path = "custom_data/AI_ML_Course_Description.pdf"
@@ -19,7 +20,12 @@ class CourseAnalyzer:
         )
         self.course_name = "Advanced AI and Machine Learning Course"
 
-    def extract_text_from_pdf(self):
+    def extract_text_from_pdf(self) -> str:
+        """Extracts raw text from PDF file
+
+        Returns:
+            str: text from PDF file as a string object
+        """
         reader = PdfReader(self.pdf_path)
         text = ""
         for page in reader.pages:
@@ -27,13 +33,31 @@ class CourseAnalyzer:
 
         return text
 
-    def generate_course_summary(self, course_description):
+    def generate_course_summary(self, course_description: str) -> str:
+        """Generates course summary for a given course description
+
+        Args:
+            course_description (str): course description to summarize
+
+        Returns:
+            str: course description summary as a string object
+        """
         prompt = prompts.course_summary_prompt(course_description)
         response = self.llm.invoke(prompt)
 
         return response.content
 
-    def analyze_profile(self, individual, summary):
+    def analyze_profile(self, individual: dict, summary: str) -> tuple[str, str]:
+        """Analyzes user profiles information, decides whether the user fits for the course or not, and explains why.
+
+        Args:
+            individual (dict): user information (name, job, social media posts, short CV)
+            summary (str): course description summary
+
+        Returns:
+            tuple[str, str]: first string is either "Yes" or "No" depending on whether the user fits for the course;
+                             second string is an explanation why the user fits for the course or not.
+        """
         profile_text = f"{individual['job_title']} {individual.get('about', '')} \
                 {individual.get('summary', '')} {' '.join(individual.get('posts', []))}"
         prompt = prompts.profile_analysis_prompt(profile_text, summary)
@@ -44,13 +68,29 @@ class CourseAnalyzer:
 
         return is_interested, explanation
 
-    def create_personalized_message(self, individual):
+    def create_personalized_message(self, individual: str) -> str:
+        """Generates a personalized message with course recommendation.
+
+        Args:
+            individual (str): user to recommend the course for
+
+        Returns:
+            str: personalized message
+        """
         prompt = prompts.personalized_message_prompt(individual, self.course_name)
         response = self.llm.invoke(prompt)
 
         return response.content
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Runs the script.
+        Dumps all the selected users with AI-generated personalized messages 
+        for each of them into a JSON file.
+
+        Returns:
+            None
+        """
         course_text = self.extract_text_from_pdf()
         summary = self.generate_course_summary(course_text)
 
--- prompts.py
+++ prompts.py
@@ -1,15 +1,46 @@
 class Prompts:
+    """Class containing prompts for course recommendations"""
 
     @staticmethod
-    def course_summary_prompt(course_description):
-        return f"Write the summary for given course description:\\n\\n{course_description}"
+    def course_summary_prompt(course_description: str) -> str:
+        """Prompt for course summary generation.
+
+        Args:
+            course_description (str): course description stored as a string
+
+        Returns:
+            str: the prompt itself
+        """
+        return (
+            f"Write the summary for given course description:\\n\\n{course_description}"
+        )
 
     @staticmethod
-    def profile_analysis_prompt(profile_text, summary):
+    def profile_analysis_prompt(profile_text: str, summary: str) -> str:
+        """Prompt for user profile analysis;
+           used to determine whether the user fits for the course or not.
+
+        Args:
+            profile_text (str): user profile description
+            summary (str): course summary
+
+        Returns:
+            str: the prompt itself
+        """
         return f"Based on the course summary {summary}, determine if the following profile is interested in this course:\\n\\n{profile_text}\\n\\nThe first sentence must be strictly only Yes or No indicating if the person fits. Then provide a brief explanation why."
 
     @staticmethod
-    def personalized_message_prompt(individual, course_name):
+    def personalized_message_prompt(individual: str, course_name: str) -> str:
+        """Prompt for personalized message generation.
+
+        Args:
+            individual (str): user name to recommend the course for
+            course_name (str): course name to recommend
+
+        Returns:
+            str: the prompt itself
+        """
         return f"Craft a personalized message to {individual['name']} to invite them to the {course_name} course based on their profile: {individual}"
+
 
 prompts = Prompts()
